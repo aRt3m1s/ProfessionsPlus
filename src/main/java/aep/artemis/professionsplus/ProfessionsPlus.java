4477c2bfa1197342801caccfa42eeba961eec9db
Christian Enano <artemis_fowl@aol.com>
2011-09-29 18:05:08 -0400

    added main files

---
+++ src/main/java/aep/artemis/professionsplus/ProfessionsPlus.java
@@ -0,0 +1,11 @@
+package aep.artemis.professionsplus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: art3m1s
+ * Date: 9/29/11
+ * Time: 5:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProfessionsPlus {
+}
